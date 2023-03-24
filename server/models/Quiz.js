43930e4022e4c6e621c793f1e710544e025c6e51
Peter Tran <pettran866@gmail.com>
2023-03-23 19:15:12 -0700

    finish Quiz model

--- server/models/Quiz.js
+++ server/models/Quiz.js
@@ -1,1 +1,26 @@
-const { Schema, model } = require('mongoose');
+const { Schema } = require('mongoose');
+const questionSchema = require('./Question');
+
+const quizSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    questions: [questionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+// get questionCount when we ask for the total number of question in the quiz
+quizSchema.virtual('questionCount').get(function () {
+  return this.questions.length;
+});
+
+const Quiz = model('Quiz', quizSchema);
+
+module.exports = Quiz;
